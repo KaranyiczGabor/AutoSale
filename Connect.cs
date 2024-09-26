40bc24f4e6f27d6d58d7ac8cccc0a2e9a9e83805
KaranyiczGabor <karanyiczg@kkszki.hu>
2024-09-26 10:17:34 +0200

    Connect class is ready to use

---
+++ Connect.cs
@@ -0,0 +1,31 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoSale
+{
+    public class Connect
+    {
+        public MySqlConnection Connection;
+        private string Host;
+        private string Database;
+        private string Username;
+        private string Password;
+        private string ConnectionString;
+
+        public Connect()
+        {
+            Host = "127.0.0.1";
+            Database = "Autook";
+            Username = "root";
+            Password = "";
+
+            ConnectionString = "SERVER=" + Host + ";DATABASE=" + Database + ";UID=" + Username + ";PASSWORD=" + Password + ";SslMode=None";
+
+            Connection = new MySqlConnection(ConnectionString);
+        }
+    }
+}
